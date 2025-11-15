7f0942e1d61856a974e1b5abaaaa391b3cc4029e
RogelioG13 <jimenezrogelio151@gmail.com>
2025-11-14 18:33:58 -0600

    Se actualizo producto repository para buscar priductos por su uso veterinario

--- src/main/java/mx/uam/ayd/proyecto/datos/ProductoRepository.java
+++ src/main/java/mx/uam/ayd/proyecto/datos/ProductoRepository.java
@@ -1,9 +1,7 @@
 package mx.uam.ayd.proyecto.datos;
 
-import mx.uam.ayd.proyecto.negocio.modelo.TipoProducto;
-import mx.uam.ayd.proyecto.negocio.modelo.MarcaProducto;
+import mx.uam.ayd.proyecto.negocio.modelo.*;
 
-import mx.uam.ayd.proyecto.negocio.modelo.Producto;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -16,7 +14,15 @@ public interface ProductoRepository extends CrudRepository <Producto, Long> {
     Optional<Producto> findByNombreAndTipoProductoAndMarcaProducto(String nombre, TipoProducto tipoProducto, MarcaProducto marcaProducto);
 
     List<Producto> findByTipoProducto(TipoProducto tipoProducto);
+
     public Producto findByIdProducto(Long idProducto);
+
     //Se agrega esta parte para la implementacion con el umbral
     List<Producto> findByCantidadStockGreaterThan(int cantidad);
+
+    //Nos permitira filtrar los productos que nos interesan cou un usoVeterinario y unidad especifico.
+    List<Producto> findByUsoVeterinarioAndUnidadProducto(UsoVeterinario usoVeterinario, UnidadProducto unidadProducto);
+
+    //Nos permitira filtrar los productos que nos interesan cou un usoVeterinario
+    List<Producto> findByUsoVeterinario(UsoVeterinario usoVeterinario);
 }
